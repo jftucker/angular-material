a954bc3051aec256f55b97c1015de8d2e748d0ee
jftucker <johntucker0686@gmail.com>
2020-11-22 20:32:51 -0800

    Custom Themes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDialogModule } from '@angular/material/dialog';
 
 import { NgModule } from '@angular/core';
 
@@ -21,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EditCourseComponent } from './edit-course/edit-course.component';
-import { CourseService } from './course.service';
+import { MatComponentsModule } from './mat-components.module';
 
 @NgModule({
   declarations: [AppComponent, EditCourseComponent],
@@ -30,19 +17,7 @@ import { CourseService } from './course.service';
     BrowserAnimationsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule,
-    MatButtonModule,
-    MatChipsModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    MatTabsModule,
-    MatDialogModule,
+    MatComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
